b5a520b99e3dace1797c0708d0a3c485a6ccb548
ojomohalex <101180584+ojomohalex@users.noreply.github.com>
2022-11-29 05:06:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 #create vpc
 
 module "vpc" {
-    source = "../modules/vpc"
+    source = "./modules/vpc"
     region = var.region
     project_name = var.project_name
     vpc_cidr = var.vpc_cidr
@@ -17,7 +17,7 @@ module "vpc" {
  #create security groups
  
  module "security-groups" {
-     source = "../modules/security-groups"
+     source = "./modules/security-groups"
      vpc_id = module.vpc.vpc_id
      
  }
@@ -25,7 +25,7 @@ module "vpc" {
  #create nat gateway
  
  module "nat-gateway" {
-     source                         = "../modules/nat-gateway"
+     source                         = "./modules/nat-gateway"
      public_subnet_az1_id           = module.vpc.public_subnet_az1_id  
      internet_gateway               = module.vpc.internet_gateway
      public_subnet_az2_id           = module.vpc.public_subnet_az2_id
@@ -40,7 +40,7 @@ module "vpc" {
  #create database
  
  module "database" {
-     source = "../modules/database"
+     source = "./modules/database"
      vpc_id = module.vpc.vpc_id
      private_data_subnet_az1_cidr     = module.vpc.private_data_subnet_az1_id
      private_data_subnet_az2_cidr     = module.vpc.private_data_subnet_az2_id
@@ -51,7 +51,7 @@ module "vpc" {
  #create EC2 instances
  
   module "ec2" {
-     source = "../modules/ec2"
+     source = "./modules/ec2"
      private_app_subnet_az1_id      = module.vpc.private_app_subnet_az1_id
      public_subnet_az1_id           = module.vpc.public_subnet_az1_id  
      private_app_subnet_az2_id      = module.vpc.private_app_subnet_az2_id
@@ -64,7 +64,7 @@ module "vpc" {
  #create load balancer
     
      module "load-balancer" {
-     source                         = "../modules/load-balancer"
+     source                         = "./modules/load-balancer"
      vpc_id                         = module.vpc.vpc_id
      public_subnet_az1_id           = module.vpc.public_subnet_az1_id
      public_subnet_az2_id           = module.vpc.public_subnet_az2_id
